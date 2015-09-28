404746ab22d968db7b78b1253ef25bdda96e5fb7
mfarah <mike.farah@ioof.com.au>
2015-09-28 13:26:02 +1000

    Added acceptance test

--- precheckin.sh
+++ precheckin.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
 gofmt -w .
 golint
 go test
+
+# acceptance test
+X=$(go run yaml.go  sample.yaml b.c)
+
+if [ $X != 2 ]
+  then
+	echo "Failed acceptance test: expected 2 but was $X"
+fi
