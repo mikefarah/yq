001925a4d4addb5425076424c270e24732631e03
Mike Farah <mikefarah@gmail.com>
2015-10-13 21:34:40 +1100

    Release task now clears build directory first

--- release.sh
+++ release.sh
@@ -3,5 +3,6 @@
 # This assumes that gonative and gox is installed as per the 'one time setup' instructions
 # at https://github.com/inconshreveable/gonative
 
+rm  build/*
 gox -output="build/{{.Dir}}_{{.OS}}_{{.Arch}}"
 
