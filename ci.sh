e3fc944709cf47fb0fa5c262713175008453ab02
Mike Farah <mike.farah@orchestrated.io>
2017-02-13 07:28:48 +1100

    Added travis config

---
+++ ci.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+golint
+go test
+go build
+
+# acceptance test
+X=$(./yaml w sample.yaml b.c 3 | ./yaml r - b.c)
+
+if [ $X != 3 ]
+  then
+  echo "Failed acceptance test: expected 2 but was $X"
+  exit 1
+fi
--- precheckin.sh
+++ precheckin.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 gofmt -w .
-golint
-go test
-go build
 
-# acceptance test
-X=$(./yaml w sample.yaml b.c 3 | ./yaml r - b.c)
-
-if [ $X != 3 ]
-  then
-	echo "Failed acceptance test: expected 2 but was $X"
-  exit 1
-fi
+./ci.sh
 
 go install
