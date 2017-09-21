3beee3f8047d3daaaebf378372f95db1a622c139
kenjones <kenjones@cisco.com>
2017-09-21 12:01:03 -0400

    Bugfix: Panic on non-string keys
    Adds check if the key is an `int` or `bool`, and converts to a string
    as part of the `toJSON` function.
    Test cases added.
    
    Resolves: #28

--- json_converter.go
+++ json_converter.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"gopkg.in/yaml.v2"
+	"strconv"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 func jsonToString(context interface{}) string {
@@ -26,7 +28,13 @@ func toJSON(context interface{}) interface{} {
 		oldMap := context.(yaml.MapSlice)
 		newMap := make(map[string]interface{})
 		for _, entry := range oldMap {
-			newMap[entry.Key.(string)] = toJSON(entry.Value)
+			if str, ok := entry.Key.(string); ok {
+				newMap[str] = toJSON(entry.Value)
+			} else if i, ok := entry.Key.(int); ok {
+				newMap[strconv.Itoa(i)] = toJSON(entry.Value)
+			} else if b, ok := entry.Key.(bool); ok {
+				newMap[strconv.FormatBool(b)] = toJSON(entry.Value)
+			}
 		}
 		return newMap
 	default:
