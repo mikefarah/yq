cf2f23d747ef93228f67d2fde34716794877e4ae
kenjones <kenjones@cisco.com>
2017-09-22 23:58:50 -0400

    Bugfix: Resolve failing acceptance test

--- scripts/acceptance.sh
+++ scripts/acceptance.sh
@@ -5,8 +5,7 @@ set -e
 # acceptance test
 X=$(./bin/yaml w ./examples/sample.yaml b.c 3 | ./bin/yaml r - b.c)
 
-if [ $X != 3 ]
-  then
+if [[ $X != 3 ]]; then
   echo "Failed acceptance test: expected 2 but was $X"
   exit 1
 fi
--- scripts/xcompile.sh
+++ scripts/xcompile.sh
@@ -3,6 +3,5 @@
 # This assumes that gonative and gox is installed as per the 'one time setup' instructions
 # at https://github.com/inconshreveable/gonative
 
-rm  build/*
 gox -output="build/{{.Dir}}_{{.OS}}_{{.Arch}}"
 
