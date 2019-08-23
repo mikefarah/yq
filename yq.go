7320b8d3c92cb13fb2db3cbb64acf8cad7c626a9
Aleksandr Sergin <inspirit1986@gmail.com>
2019-08-23 19:08:31 +0300

    fix linter error

--- yq.go
+++ yq.go
@@ -42,23 +42,23 @@ func main() {
 func MoveFile(sourcePath, destPath string) error {
     inputFile, err := os.Open(sourcePath)
     if err != nil {
-        return fmt.Errorf("Couldn't open source file: %s", err)
+        return fmt.Errorf("couldn't open source file: %s", err)
     }
     outputFile, err := os.Create(destPath)
     if err != nil {
         inputFile.Close()
-        return fmt.Errorf("Couldn't open dest file: %s", err)
+        return fmt.Errorf("couldn't open dest file: %s", err)
     }
     defer outputFile.Close()
     _, err = io.Copy(outputFile, inputFile)
     inputFile.Close()
     if err != nil {
-        return fmt.Errorf("Writing to output file failed: %s", err)
+        return fmt.Errorf("writing to output file failed: %s", err)
     }
     // The copy was successful, so now delete the original file
     err = os.Remove(sourcePath)
     if err != nil {
-        return fmt.Errorf("Failed removing original file: %s", err)
+        return fmt.Errorf("failed removing original file: %s", err)
     }
     return nil
 }
