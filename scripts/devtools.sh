f68b24323e17af6d26d857a0630f454d820f955a
Mike Farah <mike.farah@encapto.com>
2020-04-14 11:18:45 +1000

    Attempt to fix git workflow action

--- scripts/devtools.sh
+++ scripts/devtools.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-set -e
-wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.24.0
+set -ex
 go get golang.org/x/tools/cmd/goimports
+wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.24.0
