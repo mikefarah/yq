8bd54cd603bfb24ec399f31afe4f30b2edb42fec
Mike Farah <mikefarah@gmail.com>
2020-12-22 22:31:28 +1100

    fixing xcompile for git action

--- scripts/xcompile.sh
+++ scripts/xcompile.sh
@@ -11,7 +11,7 @@ rhash -r -a . -o checksums
 
 rhash --list-hashes > checksums_hashes_order
 
-find . -perm +111 -type f | xargs -I {} tar czvf {}.tar.gz {}
+find . -executable -type f | xargs -I {} tar czvf {}.tar.gz {}
 
 rm checksums_hashes_order.tar.gz
 rm checksums.tar.gz
