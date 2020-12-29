1b887e23b3a9964c525d8a302b09b56626209ad9
Mike Farah <mikefarah@gmail.com>
2020-12-30 10:40:41 +1100

    scripts/check works for local and docker build

--- scripts/check.sh
+++ scripts/check.sh
@@ -3,7 +3,12 @@
 set -o errexit
 set -o pipefail
 
-golangci-lint run --timeout=5m
+if command -v golangci-lint &> /dev/null
+then
+    golangci-lint run --timeout=5m
+else
+  ./bin/golangci-lint run --timeout=5m
+fi
 
 # ./bin/golangci-lint \
 #   --tests \
