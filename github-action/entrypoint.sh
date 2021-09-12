1fb06308cee7cd9463364113ca03031af3eccde4
YABATANIEN, Murichaduke <mail@example.com>
2021-09-12 10:07:32 +0900

    supports multiline output

--- github-action/entrypoint.sh
+++ github-action/entrypoint.sh
@@ -2,5 +2,8 @@
 set -e
 echo "::debug::\$cmd: $1"
 RESULT=$(eval "$1")
+RESULT="${RESULT//'%'/'%25'}"
+RESULT="${RESULT//$'\n'/'%0A'}"
+RESULT="${RESULT//$'\r'/'%0D'}"
 echo "::debug::\$RESULT: $RESULT"
 echo ::set-output name=result::"$RESULT"
