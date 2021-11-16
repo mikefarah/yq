fb648ceb79d01676664bd8e5a59925ed04ff9cb2
Mike Farah <mikefarah@gmail.com>
2021-11-16 16:08:37 +1100

    Load file acceptance test

--- acceptance_tests/load-file.sh
+++ acceptance_tests/load-file.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
-setUp() {
-  rm test*.yml || true
+testLoadFileNotExist() {
+  result=$(./yq e -n 'load("cat.yml")' 2>&1)
+  assertEquals 1 $?
+  assertEquals "Error: Failed to load cat.yml: open cat.yml: no such file or directory" "$result"
 }
 
-testLoadFileNotExist() {
+testLoadFileExpNotExist() {
+  result=$(./yq e -n 'load(.a)' 2>&1)
+  assertEquals 1 $?
+  assertEquals "Error: Filename expression returned nil" "$result"
 }
 
 testStrLoadFileNotExist() {
+  result=$(./yq e -n 'strload("cat.yml")' 2>&1)
+  assertEquals 1 $?
+  assertEquals "Error: Failed to load cat.yml: open cat.yml: no such file or directory" "$result"
 }
+
+testStrLoadFileExpNotExist() {
+  result=$(./yq e -n 'strload(.a)' 2>&1)
+  assertEquals 1 $?
+  assertEquals "Error: Filename expression returned nil" "$result"
+}
+
+source ./scripts/shunit2
