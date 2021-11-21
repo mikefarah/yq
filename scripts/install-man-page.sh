dbc83e01bf511c425cee78c302e49af57d472ed1
AJ ONeal <coolaj86@gmail.com>
2021-11-20 19:57:33 -0700

    feature: detect MANPATh and install there

--- scripts/install-man-page.sh
+++ scripts/install-man-page.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-cp yq.1 /usr/local/share/man/man1/.
+my_path="$(command -v yq)"
+
+if [ -z "$my_path" ]; then
+  echo 'yq' wasn't found in your PATH, so we don't know where to put the man pages.
+  echo Please update your PATH and try again.
+  exit 1
+fi
+
+# ex: ~/.local/bin/yq => ~/.local/
+my_prefix="$(dirname "$(dirname "$(command -v yq)")")"
+mkdir -p "$my_prefix/share/man/man1/"
+cp yq.1 "$my_prefix/share/man/man1/"
