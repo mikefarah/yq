46d1d6825f7b7bb968d9d24548eb7b1b90da9d80
Rob Ferguson <robert.j.ferguson@nasa.gov>
2021-11-24 08:56:21 -0600

    Include version query for tools

--- scripts/devtools.sh
+++ scripts/devtools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-go mod download golang.org/x/tools
+go mod download golang.org/x/tools@latest
 wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.37.1
 wget -O- -nv https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s v2.9.1
