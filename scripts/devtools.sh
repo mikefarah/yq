c2952d49e53d56af6543f2d6997e8708b0988bc6
Mike Farah <mikefarah@gmail.com>
2021-11-24 11:12:47 +1100

    Attempt to fix golint problem

--- scripts/devtools.sh
+++ scripts/devtools.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -ex
-go install golang.org/x/tools/cmd/goimports
-go install github.com/polyfloyd/go-errorlint
+go mod download golang.org/x/tools
 wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.37.1
 wget -O- -nv https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s v2.9.1
