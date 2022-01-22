d5b0154747807a4f9c72c79363ca75bb8fb68ec0
Mike Farah <mikefarah@gmail.com>
2022-01-22 11:56:21 +1100

    Fixing manpage path issue

--- scripts/generate-man-page-md.sh
+++ scripts/generate-man-page-md.sh
@@ -7,6 +7,10 @@ cat ./pkg/yqlib/doc/operators/headers/Main.md > man.md
 printf "\n# HOW IT WORKS\n" >> man.md
 tail -n +2 how-it-works.md >> man.md
 
-for f in ./pkg/yqlib/doc/*.md; do
+for f in ./pkg/yqlib/doc/operators/*.md; do
+  cat "$f" >> man.md
+done
+
+for f in ./pkg/yqlib/doc/usage/*.md; do
   cat "$f" >> man.md
 done
