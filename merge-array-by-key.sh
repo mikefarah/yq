4d4bd5114d7559602723f58f2d503f5f172eb97d
Mike Farah <mikefarah@gmail.com>
2022-02-09 11:49:41 +1100

    Removed temporary file

--- merge-array-by-key.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# ./yq ea '.[]'  examples/data*.yaml
-
-
-./yq ea '
-  ((.[] | {.name: .}) as $item ireduce ({}; . * $item )) as $uniqueMap
-  | ( $uniqueMap  | to_entries | .[]) as $item ireduce([]; . + $item.value)
-' examples/data*.yaml
