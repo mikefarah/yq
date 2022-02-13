mikefarah/yq

commit 4ec533bc1b029884edaba32f51dc7f9725904306

No longer have to specify eval!

yq.go
@@ -8,6 +8,17 @@ import (
 
 func main() {
 	cmd := command.New()
+
+	args := os.Args[1:]
+
+	_, _, err := cmd.Find(args)
+	if err != nil {
+		// default command when nothing matches...
+		newArgs := []string{"eval"}
+		cmd.SetArgs(append(newArgs, os.Args[1:]...))
+
+	}
+
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}

acceptance_tests/basic.sh
@@ -10,12 +10,21 @@ testBasicEvalRoundTrip() {
   assertEquals 123 "$X"
 }
 
+testBasicEvalRoundTripNoEval() {
+  ./yq -n ".a = 123" > test.yml
+  X=$(./yq '.a' test.yml)
+  assertEquals 123 "$X"
+}
+
 testBasicStdInWithOneArg() {
   ./yq e -n ".a = 123" > test.yml
   X=$(cat test.yml | ./yq e ".a")
   assertEquals 123 "$X"
 
   X=$(cat test.yml | ./yq ea ".a")
+  assertEquals 123 "$X"
+
+  X=$(cat test.yml | ./yq ".a")
   assertEquals 123 "$X"
 }
 
@@ -25,6 +34,15 @@ a: 0
 EOL
   ./yq e -i ".a = 10" test.yml
   X=$(./yq e '.a' test.yml)
+  assertEquals "10" "$X"
+}
+
+testBasicUpdateInPlaceSequenceNoEval() {
+  cat >test.yml <<EOL
+a: 0
+EOL
+  ./yq -i ".a = 10" test.yml
+  X=$(./yq '.a' test.yml)
   assertEquals "10" "$X"
 }
 
@@ -46,6 +64,12 @@ testBasicNoExitStatus() {
 testBasicExitStatus() {
   echo "a: cat" > test.yml
   X=$(./yq e -e '.z' test.yml 2&>/dev/null)
+  assertEquals 1 "$?"
+}
+
+testBasicExitStatusNoEval() {
+  echo "a: cat" > test.yml
+  X=$(./yq -e '.z' test.yml 2&>/dev/null)
   assertEquals 1 "$?"
 }
 

cmd/evalute_sequence_command.go
@@ -13,7 +13,7 @@ func createEvaluateSequenceCommand() *cobra.Command {
 	var cmdEvalSequence = &cobra.Command{
 		Use:     "eval [expression] [yaml_file1]...",
 		Aliases: []string{"e"},
-		Short:   "Apply the expression to each document in each yaml file in sequence",
+		Short:   "(default) Apply the expression to each document in each yaml file in sequence",
 		Example: `
 # Reads field under the given path for each file
 yq e '.a.b' f1.yml f2.yml 

cmd/root.go
@@ -14,6 +14,12 @@ func New() *cobra.Command {
 		Short: "yq is a lightweight and portable command-line YAML processor.",
 		Long: `yq is a portable command-line YAML processor (https://github.com/mikefarah/yq/) 
 See https://mikefarah.gitbook.io/yq/ for detailed documentation and examples.`,
+		Example: `
+# yq defaults to 'eval' command if no command is specified. See "yq eval --help" for more examples.
+cat myfile.yml | yq '.stuff' # outputs the data at the "stuff" node from "myfile.yml"
+
+yq -i '.stuff = "foo"' myfile.yml # update myfile.yml inplace
+`,
 
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if version {

commit f618d56538e51450cbed67f411ca9c6dc01a5101

Fix zsh completion
4ec533b introduced a bug which causes the 'hidden' cli parameter
`__complete` to be ignored.

Add a check for this parameter, so that it can pass to `cobra`

Signed-off-by: Tom Whitwell <tom@whi.tw>

yq.go
@@ -12,7 +12,7 @@ func main() {
 	args := os.Args[1:]
 
 	_, _, err := cmd.Find(args)
-	if err != nil {
+	if err != nil && args[0] != "__complete" {
 		// default command when nothing matches...
 		newArgs := []string{"eval"}
 		cmd.SetArgs(append(newArgs, os.Args[1:]...))

acceptance_tests/completion.sh
@@ -1,0 +1,9 @@
+#!/bin/bash
+
+testCompletionRuns() {
+    result=$(./yq __complete "" 2>&1)
+    assertEquals 0 $?
+    assertContains "$result" "Completion ended with directive:"
+}
+
+source ./scripts/shunit2