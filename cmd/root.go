1a964c50557e4fa8636d9a3233fde24c8a41e83e
justin-f-perez <justin.f.perez@gmail.com>
2022-03-19 18:54:24 -0500

    update cli help (#1146)
    - remove duplicate comment
    - add common use case (json -> idiomatic yml)

--- cmd/root.go
+++ cmd/root.go
@@ -21,7 +21,10 @@ See https://mikefarah.gitbook.io/yq/ for detailed documentation and examples.`,
 cat myfile.yml | yq '.stuff' 
 
 # update myfile.yml in place
-yq -i '.stuff = "foo"' myfile.yml # update myfile.yml inplace
+yq -i '.stuff = "foo"' myfile.yml
+
+# print contents of sample.json as idiomatic YAML
+yq -P '.' sample.json
 `,
 
 		RunE: func(cmd *cobra.Command, args []string) error {
