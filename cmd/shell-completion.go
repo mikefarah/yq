151eecf4194a7447fb3acac2cfc4a1d81279a769
Mike Farah <mikefarah@gmail.com>
2022-10-17 15:09:17 +1100

    Fixed deprecated usage of ExactArgs

--- cmd/shell-completion.go
+++ cmd/shell-completion.go
@@ -42,7 +42,7 @@ $ yq shell-completion fish > ~/.config/fish/completions/yq.fish
 `,
 	DisableFlagsInUseLine: true,
 	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
-	Args:                  cobra.ExactValidArgs(1),
+	Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		var err error = nil
 		switch args[0] {
