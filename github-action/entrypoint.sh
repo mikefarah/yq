dee0c94346ee3810e741b663c7b043dc3aa66af4
Mike Farah <mikefarah@gmail.com>
2022-10-17 15:33:12 +1100

    Updating action to new entrypoint format

--- github-action/entrypoint.sh
+++ github-action/entrypoint.sh
@@ -6,4 +6,6 @@ RESULT="${RESULT//'%'/'%25'}"
 RESULT="${RESULT//$'\n'/'%0A'}"
 RESULT="${RESULT//$'\r'/'%0D'}"
 echo "::debug::\$RESULT: $RESULT"
-echo ::set-output name=result::"$RESULT"
+# updating from 
+# https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
+echo "RESULT=$RESULT" >> $GITHUB_OUTPUT
