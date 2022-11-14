e99d5c0ab21fc9aaac2d2828be412a73930dc442
mikutas <23391543+mikutas@users.noreply.github.com>
2022-11-14 13:20:05 +0900

    Fix mis-versioning due to hardcoding cmd.Version

--- scripts/xcompile.sh
+++ scripts/xcompile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 # you may need to go install github.com/mitchellh/gox@v1.0.1 first
-
-CGO_ENABLED=0 gox -ldflags "${LDFLAGS}" -output="build/yq_{{.OS}}_{{.Arch}}" --osarch="darwin/amd64 darwin/arm64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/mips linux/mips64 linux/mips64le linux/mipsle linux/ppc64 linux/ppc64le linux/s390x netbsd/386 netbsd/amd64 netbsd/arm openbsd/386 openbsd/amd64 windows/386 windows/amd64"
+echo $VERSION
+CGO_ENABLED=0 gox -ldflags "${LDFLAGS} -X cmd.Version=${VERSION}" -output="build/yq_{{.OS}}_{{.Arch}}" --osarch="darwin/amd64 darwin/arm64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/mips linux/mips64 linux/mips64le linux/mipsle linux/ppc64 linux/ppc64le linux/s390x netbsd/386 netbsd/amd64 netbsd/arm openbsd/386 openbsd/amd64 windows/386 windows/amd64"
 
 cd build
 
