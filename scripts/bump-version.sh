a6d1a52e333dcff43aca164936601ddd09bbfe94
Mike Farah <mikefarah@gmail.com>
2022-11-15 20:08:58 +1100

    Fixing versioning code for release

---
+++ scripts/bump-version.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+if [ "$1" == "" ]; then
+  echo "Please specify at a version"
+  exit 1
+fi
+
+version=$1
+
+# validate version is in the right format
+echo $version | sed -r '/v4\.[0-9][0-9]\.[0-9][0-9]?$/!{q1}'
+
+previousVersion=$(cat cmd/version.go| sed -n 's/.*Version = "\([^"]*\)"/\1/p')
+
+echo "Updating from $previousVersion to $version"
+
+sed -i "s/\(.*Version =\).*/\1 \"$version\"/" cmd/version.go
+
+go build .
+actualVersion=$(./yq --version)
+
+if [ "$actualVersion" != "yq (https://github.com/mikefarah/yq/) version $version" ]; then
+    echo "Failed to update version.go"
+    exit 1
+else
+    echo "version.go updated"
+fi
+
+ version=$version ./yq -i '.version=strenv(version)' snap/snapcraft.yaml                                          
+
+actualSnapVersion=$(./yq '.version' snap/snapcraft.yaml)
+
+if [ "$actualSnapVersion" != "$version" ]; then
+    echo "Failed to update snapcraft"
+    exit 1
+else
+    echo "snapcraft updated"
+fi
+
+git add cmd/version.go snap/snapcraft.yaml
+git commit -m 'Bumping version'
+git tag $version
--- scripts/xcompile.sh
+++ scripts/xcompile.sh
@@ -2,7 +2,7 @@
 set -e
 # you may need to go install github.com/mitchellh/gox@v1.0.1 first
 echo $VERSION
-CGO_ENABLED=0 gox -ldflags "${LDFLAGS} -X github.com/mikefarah/yq/v4/cmd.Version=${VERSION}" -output="build/yq_{{.OS}}_{{.Arch}}" --osarch="darwin/amd64 darwin/arm64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/mips linux/mips64 linux/mips64le linux/mipsle linux/ppc64 linux/ppc64le linux/s390x netbsd/386 netbsd/amd64 netbsd/arm openbsd/386 openbsd/amd64 windows/386 windows/amd64"
+CGO_ENABLED=0 gox -ldflags "${LDFLAGS}" -output="build/yq_{{.OS}}_{{.Arch}}" --osarch="darwin/amd64 darwin/arm64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm linux/arm64 linux/mips linux/mips64 linux/mips64le linux/mipsle linux/ppc64 linux/ppc64le linux/s390x netbsd/386 netbsd/amd64 netbsd/arm openbsd/386 openbsd/amd64 windows/386 windows/amd64"
 
 cd build
 
