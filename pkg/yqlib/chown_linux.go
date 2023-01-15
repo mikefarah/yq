473be23153aed6e04ec4d687991ce8998b2e8d5b
Mike Farah <mikefarah@gmail.com>
2023-01-15 11:36:52 +1100

    Log info message when unable to chown file #1521

--- pkg/yqlib/chown_linux.go
+++ pkg/yqlib/chown_linux.go
@@ -12,7 +12,14 @@ func changeOwner(info fs.FileInfo, file *os.File) error {
 	if stat, ok := info.Sys().(*syscall.Stat_t); ok {
 		uid := int(stat.Uid)
 		gid := int(stat.Gid)
-		return os.Chown(file.Name(), uid, gid)
+
+		err := os.Chown(file.Name(), uid, gid)
+		if err != nil {
+			// this happens with snap confinement
+			// not really a big issue as users can chown
+			// the file themselves if required.
+			log.Info("Skipping chown: %v", err)
+		}
 	}
 	return nil
 }
