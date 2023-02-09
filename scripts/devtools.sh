bbb149b31e57b2ea55c1893b6868e797b78a7123
Mike Farah <mikefarah@gmail.com>
2023-02-09 18:20:11 +1100

    Bumped linter

--- scripts/devtools.sh
+++ scripts/devtools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
 go mod download golang.org/x/tools@latest
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.49.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.51.1
 wget -O- -nv https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s v2.15.0
