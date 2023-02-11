75483fe908b997f46fc728b8d1ed39fe944f5782
Mike Farah <mikefarah@gmail.com>
2023-02-11 23:54:16 +1100

    Fix github action attempt

--- scripts/devtools.sh
+++ scripts/devtools.sh
@@ -2,4 +2,4 @@
 set -ex
 go mod download golang.org/x/tools@latest
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.51.1
-wget -O- -nv https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s v2.15.0
+wget -O- -nv https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s
