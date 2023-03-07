65f4b1590ba738c5fa6f012c703e011194f5a1b9
Robert Brennan <contact@rbren.io>
2023-03-07 09:03:12 -0500

    remove logs

--- pkg/yqlib/operator_filter.go
+++ pkg/yqlib/operator_filter.go
@@ -5,12 +5,11 @@ import (
 )
 
 func filterOperator(d *dataTreeNavigator, context Context, expressionNode *ExpressionNode) (Context, error) {
-	log.Warningf("-- filterOperation")
+	log.Debugf("-- filterOperation")
 	var results = list.New()
 
 	for el := context.MatchingNodes.Front(); el != nil; el = el.Next() {
 		candidate := el.Value.(*CandidateNode)
-		log.Warningf("candidate %#v", candidate)
 		children := context.SingleChildContext(candidate)
 		splatted, err := splat(children, traversePreferences{})
 		if err != nil {
@@ -19,10 +18,6 @@ func filterOperator(d *dataTreeNavigator, context Context, expressionNode *Expre
 		filtered, err := selectOperator(d, splatted, expressionNode)
 		if err != nil {
 			return Context{}, err
-		}
-		for resultEl := filtered.MatchingNodes.Front(); resultEl != nil; resultEl = resultEl.Next() {
-			result := resultEl.Value.(*CandidateNode)
-			log.Warningf("filtered %#v", result)
 		}
 
 		selfExpression := &ExpressionNode{Operation: &Operation{OperationType: selfReferenceOpType}}
