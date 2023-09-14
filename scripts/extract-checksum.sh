8067dc9e774ed10614de27946b44224d5ca10c93
Taisuke Miyazaki <imishinist@gmail.com>
2023-09-14 11:55:44 +0900

    fix typo

--- scripts/extract-checksum.sh
+++ scripts/extract-checksum.sh
@@ -47,7 +47,7 @@ fi
 
 grepMatch=$(grep -m 1 -n "$1" checksums_hashes_order)
 if [ "$grepMatch" == "" ]; then
-  echo "Could not find hash algorith '$1' in checksums_hashes_order"
+  echo "Could not find hash algorithm '$1' in checksums_hashes_order"
   exit 1
 fi
 
