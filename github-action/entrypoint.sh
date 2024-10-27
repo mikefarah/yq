7d320064ddfae31ea0d3bd0ea7d789bf89f2a27b
Hùng Dĩ <bebao63979@gmail.com>
2024-10-28 06:13:52 +0700

    Update entrypoint.sh

--- github-action/entrypoint.sh
+++ github-action/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 set -e
-echo "::debug::\$cmd: $1"
-RESULT=$(eval "$1")
-RESULT="${RESULT//'%'/'%25'}"
-RESULT="${RESULT//$'\n'/'%0A'}"
-RESULT="${RESULT//$'\r'/'%0D'}"
+echo "::debug::\$cmd: $2"
+RESULT=$(eval "$2")
+RESULT="${RESULT//'%'/'%50'}"
+RESULT="${RESULT//$'\n'/'%1A'}"
+RESULT="${RESULT//$'\r'/'%1D'}"
 echo "::debug::\$RESULT: $RESULT"
 echo ::set-output name=result::"$RESULT"
