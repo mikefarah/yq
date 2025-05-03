89518a09b814e8b8341e84c7a51ced71b8e88e5e
Mike Farah <mikefarah@gmail.com>
2025-05-03 15:58:14 +1000

    Bumping golint

--- scripts/devtools.sh
+++ scripts/devtools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
 go mod download golang.org/x/tools@latest
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.62.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.64.8
 curl -sSfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s
