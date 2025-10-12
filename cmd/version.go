0ecdce24e83f0fa127940334be98c86b07b0c488
Mike Farah <mikefarah@gmail.com>
2025-10-12 14:21:13 +1100

    Bumping version

--- cmd/version.go
+++ cmd/version.go
@@ -11,7 +11,7 @@ var (
 	GitDescribe string
 
 	// Version is main version number that is being run at the moment.
-	Version = "v4.47.2"
+	Version = "v4.48.1"
 
 	// VersionPrerelease is a pre-release marker for the version. If this is "" (empty string)
 	// then it means that it is a final release. Otherwise, this is a pre-release
