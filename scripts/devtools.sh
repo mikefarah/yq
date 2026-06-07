f9b0d7e45d7bc4a78546403b28f28dc785d48340
Mike Farah <mikefarah@gmail.com>
2026-06-07 19:49:39 +1000

    Pinning using hashes

--- scripts/devtools.sh
+++ scripts/devtools.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
 go mod download golang.org/x/tools@latest
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/v2.11.3/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.11.3
-curl -sSfL https://raw.githubusercontent.com/securego/gosec/v2.22.11/install.sh | sh -s v2.22.11
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/6008b81b81c690c046ffc3fd5bce896da715d5fd/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.11.3
+curl -sSfL https://raw.githubusercontent.com/securego/gosec/424fc4cd9c82ea0fd6bee9cd49c2db2c3cc0c93f/install.sh | sh -s v2.22.11
