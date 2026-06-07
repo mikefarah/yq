91a166e8d8dd812578c08ed60dc8cb1f1ca4a0d2
Mike Farah <mikefarah@gmail.com>
2026-06-07 19:57:15 +1000

    Pinning using hashes

--- scripts/xcompile.sh
+++ scripts/xcompile.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-# You may need to go install github.com/goreleaser/goreleaser/v2@latest first
+# You may need to go install github.com/goreleaser/goreleaser/v2@v2.16.0 first
 GORELEASER="goreleaser build --clean"
 if [ -z "$CI" ] || [[ "${GITHUB_REF_NAME:-}" == draft-* ]]; then
   GORELEASER+=" --snapshot"
