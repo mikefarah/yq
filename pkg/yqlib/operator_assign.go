9be91aebf994b02c6825511e86d5da7dbe9ef0ff
RRXXZZYY <RRXXZZYY@users.noreply.github.com>
2026-09-16 08:07:59 +0800

    fix: allow attribute assignments to create paths

--- pkg/yqlib/operator_assign.go
+++ pkg/yqlib/operator_assign.go
@@ -73,7 +73,7 @@ func assignUpdateOperator(d *dataTreeNavigator, context Context, expressionNode 
 // does not update content or values
 func assignAttributesOperator(d *dataTreeNavigator, context Context, expressionNode *ExpressionNode) (Context, error) {
 	log.Debug("getting lhs matching nodes for update")
-	lhs, err := d.GetMatchingNodes(context, expressionNode.LHS)
+	lhs, err := d.GetMatchingNodes(context.WritableClone(), expressionNode.LHS)
 	if err != nil {
 		return Context{}, err
 	}
