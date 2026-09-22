e71959e90f9efee38f07420def50e4724458180e
Mike Farah <mikefarah@gmail.com>
2026-09-22 06:35:13 +0000

    updating tools

--- scripts/devtools.sh
+++ scripts/devtools.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -ex
 go mod download golang.org/x/tools@v0.44.0
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/6008b81b81c690c046ffc3fd5bce896da715d5fd/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.11.3
-curl -sSfL https://raw.githubusercontent.com/securego/gosec/424fc4cd9c82ea0fd6bee9cd49c2db2c3cc0c93f/install.sh | sh -s v2.22.11
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/27774aaf853a4fd21f1dd5e69439459dc1b26e68/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.13.2
+curl -sSfL https://raw.githubusercontent.com/securego/gosec/deb54465fea23d19a77f037e11e6589021f8501d/install.sh | sh -s v2.29.0
 
 TYPOS_VERSION=v1.47.2
 TYPOS_OS=$(uname -s | tr '[:upper:]' '[:lower:]')
